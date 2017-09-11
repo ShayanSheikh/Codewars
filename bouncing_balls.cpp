8081a346ca6f7fec6b7c298887503c9787a3dcfb
ShayanSheikh <31255303+ShayanSheikh@users.noreply.github.com>
2017-09-11 16:25:14 -0400

    Update bouncing_balls.cpp
    A child plays with a ball on the nth floor of a big building. The height of this floor is known. 
    He lets out the ball. The ball bounces for example to two-thirds of its height. His mother looks out 
    of a window that is 1.5 meters from the ground. How many times will the mother see the ball either 
    falling or bouncing in front of the window?

--- bouncing_balls.cpp
+++ bouncing_balls.cpp
@@ -16,8 +16,5 @@ public:
     }
 };
 
-/* A child plays with a ball on the nth floor of a big building. The height of this floor is known. 
-He lets out the ball. The ball bounces for example to two-thirds of its height. His mother looks out 
-of a window that is 1.5 meters from the ground. How many times will the mother see the ball either 
-falling or bouncing in front of the window? */
+
 
